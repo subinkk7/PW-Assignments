0cf3b1be6a43f4a02a3ac0a35367eeadcf45379c
subinkk7 <subinkk1711@gmail.com>
2026-08-05 23:44:02 +0530

    Add files via upload

---
+++ Week1day3/CA3-NumberType.js
@@ -0,0 +1,13 @@
+function num(number) 
+{
+    if (number>0)
+    {
+        return "Positive number"
+    } else if(number<0){
+        return "Negative number"
+    } else{
+        return "Neutral"
+    }
+
+}
+console.log(num(-1));
